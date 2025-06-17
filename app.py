96d2db7468c1f11ef0e4a5317e30fc63e3ea30f2
Michael Okoro <michaelokoro1@gmail.com>
2025-06-17 09:04:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -657,14 +657,14 @@ def main_app():
                     )
                     
                     try:
-                        # Display model information
-                        # model_type = "LightGBM"
-                        # st.markdown(f"""
-                        # ### Model Information
-                        # **Algorithm:** {model_type}  
-                        # **Training Accuracy:** 84.59%  
-                        # **Training Weighted F1-Score:** 82.09%
-                        # """)
+                        #Display model information
+                        model_type = "LightGBM"
+                        st.markdown(f"""
+                        ### Model Information
+                        **Algorithm:** {model_type}  
+                        **Training Accuracy:** 84.59%  
+                        **Training Weighted F1-Score:** 82.09%
+                        """)
                         
                         # 1. Scale the data
                         input_scaled = artifacts['scaler'].transform(input_data)
